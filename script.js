2c8a47360c0f85c67c29bb3b8e922cf7abec9e2e
adeliya21 <adeliya.salieva@gmail.com>
2024-01-28 12:17:15 -0800

    handleOperator

--- script.js
+++ script.js
@@ -14,11 +14,17 @@ document.addEventListener('DOMContentLoaded', function(){
     let operators = document.querySelectorAll('.operator');
 
     let previousScreen = document.querySelector('.previous');
-    let curretnScreen = document.querySelector('.current');
+    let currentScreen = document.querySelector('.current');
 
     numbers.forEach((number) => number.addEventListener('click', function(e) {
         handleNumber(e.target.textContent) // change the global varibale to store input number
-        curretnScreen.textContent = currentValue; // update the screen to show value of global variable
+        currentScreen.textContent = currentValue; // update the screen to show value of global variable
+    }))
+
+    operators.forEach((op) => op.addEventListener('click', function(e) {
+        handleOperator(e.target.textContent); // change the global variable to store operator and update previous and current values
+        previousScreen.textContent = previousValue + ' ' + operator; // update the previous screen to show previous number and operator
+        currentScreen.textContent = currentValue; // update the current screen to current number which is empty
     }))
 })
 
@@ -27,4 +33,11 @@ function handleNumber(num) {
     if (currentValue.length < 5) { 
         currentValue += num;
     }
+}
+
+// change the global variable to store operator and update previous and current values
+function handleOperator(op) {
+    operator = op;
+    previousValue = currentValue;
+    currentValue = '';
 }

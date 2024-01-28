ae846844cf73593b1aed1138d3a5d6d7f084e54f
adeliya21 <adeliya.salieva@gmail.com>
2024-01-28 11:46:38 -0800

    store HTML components in JS

---
+++ script.js
@@ -0,0 +1,18 @@
+// Global variables
+let operator = '';
+let currentValue = '';
+let previousValue = '';
+
+
+// Store all HTML components in JS
+document.addEventListener('DOMContentLoaded', function(){
+    let clear = document.querySelector('#clear-btn');
+    let equal = document.querySelector('.equal');
+    let deciaml = document.querySelector('.deciaml');
+
+    let numbers = document.querySelector('.number');
+    let operators = document.querySelector('.operator');
+
+    let previousScreen = document.querySelector('.previous');
+    let curretnScreen = document.querySelector('.current');
+})
